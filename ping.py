ab321d7e70237176beb87d2c784faf5a3addec73
wgq2633 <wgq2633@wgq2633.com>
2017-06-03 12:10:07 +0800

    resolve bugs for timeout-rate while ip/host never has a response.

--- ping.py
+++ ping.py
@@ -219,12 +219,15 @@ class Printing(threading.Thread):
                 ipid=ipid+1
                 print "%s\033[%d;0H\033[K" % (style,stdout_linenum),
                 stdout_linenum+=1
-                tmp_amount_updates=(info.amount_updates if info.amount_updates>0 else 1)
+                
+                timeout_rate=100.0
+                if info.amount_updates>0:
+                    timeout_rate=100.0*info.total_timeouts/info.amount_updates
                 print "%-25s %-10s DELAY:%s\tERRORS: %d\tTIMEOUTS: %d/%-3.2f%%]\033[0m" % \
                       (info.hostname, 
                       ("=" * (info.ok_counter%10) + ">" ),
                       p_helper1(info.curr_delay, info.highest_delay, info.calc_avg_delay()), info.total_errors,
-                       info.total_timeouts, 100.0*info.total_timeouts/tmp_amount_updates )
+                       info.total_timeouts, timeout_rate)
 
             if self.msg_queue.len == 0 and not run_loop:
                 loop = False
